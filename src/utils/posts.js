60ec85dfb81170d043fb3169e81e098a9fbcb99b
Keshav Rao <keshavrao250@gmail.com>
2025-01-10 01:18:48 -0800

    feat: migrate posts to markdown and add utils

---
+++ src/utils/posts.js
@@ -0,0 +1,51 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'content/posts');
+
+export function getAllPosts() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames
+    .filter(fileName => fileName.endsWith('.md'))
+    .map(fileName => {
+      // Remove ".md" from file name to get id
+      const id = fileName.replace(/\.md$/, '');
+
+      // Read markdown file as string
+      const fullPath = path.join(postsDirectory, fileName);
+      const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+      // Use gray-matter to parse the post metadata section
+      const { data, content } = matter(fileContents);
+
+      // Combine the data with the id
+      return {
+        id,
+        content,
+        ...data
+      };
+    });
+
+  // Sort posts by date
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
+export function getPostBySlug(slug) {
+  const fullPath = path.join(postsDirectory, `${slug}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const { data, content } = matter(fileContents);
+
+  return {
+    slug,
+    content,
+    ...data
+  };
+}
