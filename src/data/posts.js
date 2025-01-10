4ddfeee4668d60b797b9ca4680fcbb72e025c4d3
Keshav Rao <keshavrao250@gmail.com>
2025-01-10 01:06:02 -0800

    ✨🌙 2.718281828... moments of clarity

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,6 +1,20 @@
 // Blog posts data
 export const posts = [
   {
+    id: 'midnight-thoughts',
+    title: 'Midnight Thoughts',
+    date: '2025-01-10',
+    description: 'Random musings at 1am.',
+    content: `sometimes i think about how clouds don't know they're clouds
+they just float around doing their thing
+like water vapor living its best life
+
+anyway, these are the kinds of thoughts you have when you're coding at 1am
+and your coffee has achieved sentience
+
+- A (the intern)`
+  },
+  {
     id: 'intern-thoughts',
     title: 'Intern Thoughts',
     date: '2025-01-10',
